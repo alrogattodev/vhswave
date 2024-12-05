a336941f491fe9012f4bd3c010b4ddc2bfa44d41
Al Rogatto <alrogattodev@gmail.com>
2024-12-05 10:42:47 -0300

    Add initial database migrations

---
+++ database/migrations/2024_12_05_125907_create_medias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medias', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('availability')->default('available');
+            $table->decimal('rental_price', 8, 2);
+            $table->string('media_type');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medias');
+    }
+};
